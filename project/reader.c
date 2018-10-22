b4247f3eecd130f6e5ae71c35533a908e47cfd37
root <root@gnu34.netlab.fe.up.pt>
2018-10-22 15:00:39 +0100

    first implementations

---
+++ project/reader.c
@@ -0,0 +1,255 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+
+#define BAUDRATE 	       B38400
+#define _POSIX_SOURCE 	 1 /* POSIX compliant source */
+#define FALSE 		       0
+#define TRUE 		         1
+
+#define FLAG 		      0x7e
+#define ESCAPE 		    0x7d
+#define A_RECEIVED 	  0x03
+#define A_CONFIRM	    0x01
+#define C_RECEIVED	  0x03
+#define C_CONFIRM 	  0x07
+#define BCC 		      A^C
+
+#define MAX_SIZE	255
+
+volatile int stuffedByte = 0, dataIdx = 4;
+
+volatile int RECEIVED = FALSE;
+
+typedef enum { INITIAL, STATE_FLAG, STATE_A, STATE_C, STATE_BCC , STATE_FINAL } State;
+/* state machine for handling received bytes */
+void handle_setup(State* state, unsigned char byte, unsigned char setup[]) {
+	switch(*state) {
+		case INITIAL:
+			if (byte == FLAG) {
+				setup[0] = FLAG;
+				*state = STATE_FLAG;
+			}
+			break;
+		case STATE_FLAG:
+			if (byte == A_RECEIVED) {
+				setup[1] = A_RECEIVED;
+				*state = STATE_A;
+			} else if (byte == FLAG) {
+				setup[0] = FLAG;
+				*state = STATE_FLAG;
+			} else {
+				*state = INITIAL;
+			}
+			break;
+		case STATE_A:
+			if (byte == C_RECEIVED) {
+				setup[2] = C_RECEIVED;
+				*state = STATE_C;
+			} else if (byte == FLAG) {
+				setup[0] = FLAG;
+				*state = STATE_FLAG;
+			} else {
+				*state = INITIAL;
+			}
+			break;
+		case STATE_C:
+			if (byte == setup[1]^setup[2]) {
+				setup[3] = setup[1]^setup[2];
+				*state = STATE_BCC;
+			} else if (byte == FLAG) {
+				setup[0] = FLAG;
+				*state = STATE_FLAG;
+			} else {
+				*state = INITIAL;
+			}
+			break;
+		case STATE_BCC:
+			if (byte == FLAG) {
+				RECEIVED = TRUE;
+				setup[4] = FLAG;
+			}
+			else
+				*state = INITIAL;
+			break;
+		default:
+			break;
+	}
+}
+
+void handle_frame(State* state, unsigned char byte, unsigned char frameI[]) {
+  switch(*state) {
+    case INITIAL:
+    if (byte == FLAG) {
+      frameI[0] = FLAG;
+      *state = STATE_FLAG;
+    }
+    break;
+    case STATE_FLAG:
+    if (byte == A_RECEIVED) {
+      frameI[1] = A_RECEIVED;
+      *state = STATE_A;
+    } else if (byte == FLAG) {
+      frameI[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_A:
+    if (byte == C_RECEIVED) {
+      frameI[2] = C_RECEIVED;
+      *state = STATE_C;
+    } else if (byte == FLAG) {
+      frameI[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_C:
+    if (byte == frameI[1]^frameI[2]) {
+      frameI[3] = frameI[1]^frameI[2];
+      *state = STATE_BCC;
+    } else if (byte == FLAG) {
+      frameI[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_BCC:
+    if (byte == ESCAPE) {
+      stuffedByte = 1;
+    } else if (stuffedByte) {
+      frameI[dataIdx++] = byte ^ 0x20;
+      stuffedByte = 0;
+    } else {
+      frameI[dataIdx++] = byte;
+    }
+    break;
+    case STATE_FINAL:
+    {
+      unsigned char bcc2 = frameI[4];
+
+      int i;
+      for (i = 5; i < dataIdx; i++) {
+        bcc2 ^= frameI[i];
+      }
+
+      if (frameI[dataIdx] == bcc2) {
+        frameI[dataIdx+1] = FLAG;
+        RECEIVED = TRUE;
+      } else {
+        printf("BCC2 incompatible\n");
+      }
+      break;
+    }
+    default:
+    break;
+  }
+}
+
+void print_arr(unsigned char arr[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+  printf("%x ",  arr[i]);
+  printf("\n");
+}
+
+int llopen(int fd, int flag)
+{
+  unsigned char buf[255], setup[5], ua[5];
+
+  State state = INITIAL;
+  while (RECEIVED == FALSE) {       		   /* loop for input */
+    read(fd,buf,1);     			             /* returns after 1 char has been input */
+    handle_setup(&state, buf[0], setup);	 /* handle byte received */
+  }
+  printf("SET received: ");
+  print_arr(setup, 5);
+
+  /* send confirmation */
+  int i;
+  for (i = 0; i < 5; i++) {
+    if (i == 1)
+    ua[i] = A_CONFIRM;
+    else if (i == 2)
+    ua[i] = C_CONFIRM;
+    else if (i == 3)
+    ua[i] = ua[1]^ua[2];
+    else
+    ua[i] = setup[i];
+  }
+  write(fd,ua,5);
+  printf("UA sent: ");
+  print_arr(ua, 5);
+}
+
+int llwrite(int fd, char* buffer, int length)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+  int fd,c, res;
+  struct termios oldtio,newtio;
+  unsigned char buf[255], frameI[MAX_SIZE];
+
+  if ((argc < 2) ||
+  ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
+
+  /*
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
+
+  if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
+
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
+
+  /*
+  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+  leitura do(s) pr�ximo(s) caracter(es)
+  */
+  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 char is received */
+
+  tcflush(fd, TCIOFLUSH);
+
+  if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+  printf("New termios structure set\n");
+
+  llopen(fd, 0);
+
+  sleep(2);
+
+  tcsetattr(fd,TCSANOW,&oldtio);
+  close(fd);
+  return 0;
+}
---
+++ project/writer.c
@@ -0,0 +1,271 @@
+/* INITIAL READER */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <signal.h>
+
+#define BAUDRATE      B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE         0
+#define TRUE          1
+
+#define FLAG      0x7E
+#define ESCAPE    0x7d
+#define A         0x03
+#define C         0x40
+
+#define C_CONFIRM 0x07
+
+#define DSIZE     10
+
+volatile int RECEIVED_UA=FALSE;
+
+int timeout = 0, n_tries = 0;
+
+typedef enum { INITIAL, STATE_FLAG, STATE_A, STATE_C, STATE_BCC } State;
+/* state machine for handling received bytes */
+void handle_setup(State* state, unsigned char byte, unsigned char setup[]) {
+  switch(*state) {
+    case INITIAL:
+    if (byte == FLAG) {
+      setup[0] = FLAG;
+      *state = STATE_FLAG;
+    }
+    break;
+    case STATE_FLAG:
+    if (byte == A) {
+      setup[1] = A;
+      *state = STATE_A;
+    } else if (byte == FLAG) {
+      setup[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_A:
+    if (byte == C) {
+      setup[2] = C;
+      *state = STATE_C;
+    } else if (byte == FLAG) {
+      setup[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_C:
+    if (byte == setup[1]^setup[2]) {
+      setup[3] = setup[1]^setup[2];
+      *state = STATE_BCC;
+    } else if (byte == FLAG) {
+      setup[0] = FLAG;
+      *state = STATE_FLAG;
+    } else {
+      *state = INITIAL;
+    }
+    break;
+    case STATE_BCC:
+    if (byte == FLAG) {
+      RECEIVED_UA = TRUE;
+      setup[4] = FLAG;
+    }
+    else
+    *state = INITIAL;
+    break;
+    default:
+    break;
+  }
+}
+
+void shiftRight(char* buffer, int position, int length)
+{
+  char temp = buffer[position];
+  char temp2;
+  buffer[position]=0;
+
+  int i;
+  for(i = position ; i <= length ; i++){
+    temp2 = buffer[i+1];
+    buffer[i+1] = temp;
+    temp = temp2;
+  }
+}
+
+void print_arr(unsigned char arr[], int size)
+{
+  int i;
+  for (i = 0; i < size; i++)
+  printf("%x ", arr[i]);
+  printf("\n");
+}
+
+/* timeout alarm */
+void timeout_alarm()
+{
+  timeout = 1;
+}
+
+
+int llopen(int fd, int flag)
+{
+  unsigned char ua[5], setup[5], buf[255];
+
+  /* installs alarm handler */
+	(void) signal(SIGALRM, timeout_alarm);
+
+	State state = INITIAL;
+
+	setup[0] = FLAG;
+	setup[1] = A;
+	setup[2] = C;
+	setup[3] = setup[1]^setup[2]; //BCC
+
+  while (!RECEIVED_UA && n_tries < 3) {
+
+		/* writes SET to serial port */
+		write(fd,setup,5);
+		printf("SET sent: ");
+		print_arr(setup, 5);
+		alarm(3);
+
+		while (!timeout) {
+
+			if (RECEIVED_UA) {
+				printf("UA received: ");
+				print_arr(ua, 5);
+				break;
+			}
+
+			read(fd,buf,1);
+			handle_setup(&state, buf[0], ua);
+    }
+
+		timeout = 0;
+		n_tries++;
+	}
+
+  return 0;
+}
+
+int llwrite(int fd, char* buffer, int length)
+{
+    unsigned char dados[DSIZE];
+
+    /*fileD = open(argv[2], O_RDONLY);
+    if (fd <0) {perror(argv[2]); exit(-1); }
+
+    printf("File opened\n");
+    unsigned char setup[255];
+    int i = 0;
+    int numShifts = 0;
+
+    while(i<DSIZE){
+
+      if(i==5)
+      dados[i]=FLAG;
+      else if(i==8 )
+      dados[i]=ESCAPE;
+      else 	dados[i]=0x45;
+
+      bcc2 = bcc2 ^ dados[i];
+      i++;
+    }
+
+    //	check FLAGs in dados				o size aumenta nr de flags que houver
+
+    i = 0;
+    while(i < DSIZE){
+      if(dados[i] == FLAG){
+        shiftRight(dados, i, DSIZE);
+        dados[i]=ESCAPE;
+        dados[i+1]=0x5e;
+        numShifts++;
+      } else if(dados[i] == ESCAPE){
+        shiftRight(dados, i, DSIZE);
+        dados[i]=ESCAPE;
+        dados[i+1]=0x5d;
+        numShifts++;
+      }
+
+      i++;
+    }
+
+
+    i = 0;
+    while(i<DSIZE+numShifts){
+      setup[i+4]=dados[i];
+      i++;
+    }
+
+    setup[DSIZE+4+numShifts] = bcc2;
+    setup[DSIZE+5+numShifts] = FLAG;
+
+    close(fileD)*/
+
+    return 0;
+}
+
+
+int main(int argc, char** argv)
+{
+  int fd;
+  struct termios oldtio, newtio;
+
+  if ((argc < 3) ||
+  ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort File\n\tex: nserial /dev/ttyS1 Penguin.gif\n");
+    exit(1);
+  }
+
+  /*
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
+
+  if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
+
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
+
+  /*
+  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+  leitura do(s) próximo(s) caracter(es)
+  */
+  newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 0;   /* blocking read until 1 char is received */
+
+  tcflush(fd, TCIOFLUSH);
+
+  if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+  printf("New termios structure set\n");
+
+  llopen(fd, 1);
+
+  sleep(2);
+
+  tcsetattr(fd,TCSANOW,&oldtio);
+
+  close(fd);
+  return 0;
+}
