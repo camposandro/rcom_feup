4f45c34759996fdedec4dbc8a42cc7ac75ead8b0
peixaxa <cona@feup.com>
2018-10-23 19:33:14 +0100

    llclose implementation

--- project/writer.c
+++ project/writer.c
@@ -1,5 +1,3 @@
-/* INITIAL READER */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -15,16 +13,19 @@
 #define FALSE         0
 #define TRUE          1
 
-#define FLAG      0x7E
-#define ESCAPE    0x7d
-#define A         0x03
-#define C         0x40
+#define TRIES         3
 
-#define C_CONFIRM 0x07
+#define FLAG          0x7E
+#define ESCAPE        0x7d
+#define A             0x03
+#define C             0x40
+#define C_DISC        0x0B
+
+#define C_UA          0x07
 
 #define DSIZE     10
 
-volatile int RECEIVED_UA=FALSE;
+volatile int RECEIVED = FALSE;
 
 int timeout = 0, n_tries = 0;
 
@@ -73,7 +74,7 @@ void handle_setup(State* state, unsigned char byte, unsigned char setup[]) {
     break;
     case STATE_BCC:
     if (byte == FLAG) {
-      RECEIVED_UA = TRUE;
+      RECEIVED = TRUE;
       setup[4] = FLAG;
     }
     else
@@ -114,106 +115,186 @@ void timeout_alarm()
 
 int llwrite(int fd, char* buffer, int length)
 {
-    unsigned char dados[DSIZE];
+  unsigned char dados[DSIZE];
 
-    /*fileD = open(argv[2], O_RDONLY);
-    if (fd <0) {perror(argv[2]); exit(-1); }
+  /*fileD = open(argv[2], O_RDONLY);
+  if (fd <0) {perror(argv[2]); exit(-1); }
 
-    printf("File opened\n");
-    unsigned char setup[255];
-    int i = 0;
-    int numShifts = 0;
+  printf("File opened\n");
+  unsigned char setup[255];
+  int i = 0;
+  int numShifts = 0;
 
-    while(i<DSIZE){
+  while(i<DSIZE){
 
-      if(i==5)
-      dados[i]=FLAG;
-      else if(i==8 )
-      dados[i]=ESCAPE;
-      else 	dados[i]=0x45;
+  if(i==5)
+  dados[i]=FLAG;
+  else if(i==8 )
+  dados[i]=ESCAPE;
+  else 	dados[i]=0x45;
 
-      bcc2 = bcc2 ^ dados[i];
-      i++;
-    }
-
-    //	check FLAGs in dados				o size aumenta nr de flags que houver
-
-    i = 0;
-    while(i < DSIZE){
-      if(dados[i] == FLAG){
-        shiftRight(dados, i, DSIZE);
-        dados[i]=ESCAPE;
-        dados[i+1]=0x5e;
-        numShifts++;
-      } else if(dados[i] == ESCAPE){
-        shiftRight(dados, i, DSIZE);
-        dados[i]=ESCAPE;
-        dados[i+1]=0x5d;
-        numShifts++;
-      }
-
-      i++;
-    }
-
-
-    i = 0;
-    while(i<DSIZE+numShifts){
-      setup[i+4]=dados[i];
-      i++;
-    }
-
-    setup[DSIZE+4+numShifts] = bcc2;
-    setup[DSIZE+5+numShifts] = FLAG;
-
-    close(fileD)*/
-
-    return 0;
+  bcc2 = bcc2 ^ dados[i];
+  i++;
 }
 
-int llopen(int fd, int flag)
+//	check FLAGs in dados				o size aumenta nr de flags que houver
+
+i = 0;
+while(i < DSIZE){
+if(dados[i] == FLAG){
+shiftRight(dados, i, DSIZE);
+dados[i]=ESCAPE;
+dados[i+1]=0x5e;
+numShifts++;
+} else if(dados[i] == ESCAPE){
+shiftRight(dados, i, DSIZE);
+dados[i]=ESCAPE;
+dados[i+1]=0x5d;
+numShifts++;
+}
+
+i++;
+}
+
+
+i = 0;
+while(i<DSIZE+numShifts){
+setup[i+4]=dados[i];
+i++;
+}
+
+setup[DSIZE+4+numShifts] = bcc2;
+setup[DSIZE+5+numShifts] = FLAG;
+
+close(fileD)*/
+
+return 0;
+}
+
+int llopen(char* port, int flag)
 {
+  int n_tries = 0;
+
   unsigned char ua[5], setup[5], buf[255];
 
+  /*
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
+  */
+  int fd = open(port, O_RDWR | O_NOCTTY);
+  if (fd < 0) { perror(port); exit(-1); }
+
   /* installs alarm handler */
-	(void) signal(SIGALRM, timeout_alarm);
+  (void) signal(SIGALRM, timeout_alarm);
 
-	State state = INITIAL;
+  State state = INITIAL;
 
-	setup[0] = FLAG;
-	setup[1] = A;
-	setup[2] = C;
-	setup[3] = setup[1]^setup[2]; //BCC
+  setup[0] = FLAG;
+  setup[1] = A;
+  setup[2] = C;
+  setup[3] = setup[1]^setup[2]; //BCC
 
-  while (!RECEIVED_UA && n_tries < 3) {
+  while (!RECEIVED && n_tries < TRIES) {
 
-		/* writes SET to serial port */
-		write(fd,setup,5);
-		printf("SET sent: ");
-		print_arr(setup, 5);
-		alarm(3);
+    /* writes SET to serial port */
+    write(fd,setup,5);
+    printf("SET sent: ");
+    print_arr(setup, 5);
+    alarm(3);
 
-		while (!timeout) {
+    while (!timeout) {
 
-			if (RECEIVED_UA) {
-				printf("UA received: ");
-				print_arr(ua, 5);
-				break;
-			}
+      if (RECEIVED_UA) {
+        printf("UA received: ");
+        print_arr(ua, 5);
+        break;
+      }
 
-			read(fd,buf,1);
-			handle_setup(&state, buf[0], ua);
+      read(fd,buf,1);
+      handle_setup(&state, buf[0], ua);
     }
 
-		timeout = 0;
-		n_tries++;
-	}
+    timeout = 0;
+    n_tries++;
+  }
 
-  return 0;
+  return fd;
+}
+
+int llclose(int fd)
+{
+  RECEIVED = FALSE;
+
+  int n_tries = 0;
+
+  unsigned char ua[5], disc[5], buf[255];
+
+  /* installs alarm handler */
+  (void) signal(SIGALRM, timeout_alarm);
+
+  State state = INITIAL;
+
+  disc[0] = FLAG;
+  disc[1] = A;
+  disc[2] = C_DISC;
+  disc[3] = disc[1]^disc[2]; //BCC
+
+  while (!RECEIVED && n_tries < TRIES) {
+
+    /* writes SET to serial port */
+    write(fd,disc,5);
+    printf("DISC sent: ");
+    print_arr(disc, 5);
+    alarm(3);
+
+    while (!timeout) {
+
+      if (RECEIVED) {
+        printf("DISC received: ");
+        print_arr(disc, 5);
+        break;
+      }
+
+      read(fd,buf,1);
+      handle_setup(&state, buf[0], disc);
+    }
+
+    timeout = 0;
+    n_tries++;
+  }
+
+  ua[0] = FLAG;
+  ua[1] = A;
+  ua[2] = C_UA;
+  ua[3] = ua[1]^ua[2];
+
+  write(fd,ua,5);
+
+  close(fd);
+}
+
+FILE* openfile(char* filepath)
+{
+  FILE* img = fopen(filepath, "r");
+  if (img == NULL) {
+    perror("Could not read file!\n");
+    return NULL;
+  }
+
+  return img;
+}
+
+int writefile(int fd, FILE* file)
+{
+   while (!feof(img)) {
+    char c = fgetc(img);
+    write(fd,c,1);
+    printf("%c",c);
+  }
 }
 
 int main(int argc, char** argv)
 {
-  int fd;
   struct termios oldtio, newtio;
 
   if ((argc < 3) ||
@@ -223,12 +304,7 @@ int main(int argc, char** argv)
     exit(1);
   }
 
-  /*
-  Open serial port device for reading and writing and not as controlling tty
-  because we don't want to get killed if linenoise sends CTRL-C.
-  */
-  fd = open(argv[1], O_RDWR | O_NOCTTY );
-  if (fd <0) {perror(argv[1]); exit(-1); }
+  int fd = llopen(argv[1], 1);
 
   if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
     perror("tcgetattr");
@@ -258,12 +334,15 @@ int main(int argc, char** argv)
   }
   printf("New termios structure set\n");
 
-  llopen(fd, 1);
+  FILE* file = openfile("penguin.gif");
+  if (file != NULL)
+    writefile(fd,file);
 
   sleep(2);
 
+  llclose(fd);
+
   tcsetattr(fd,TCSANOW,&oldtio);
 
-  close(fd);
   return 0;
 }
